56035057b525dbfcf9bc955f1a392d4521ca2d99
JakesProjects1 <jacob@kagon.net>
2021-05-11 20:59:28 -0400

    updating navbar

--- components/Container.js
+++ components/Container.js
@@ -1,10 +1,10 @@
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { useState, useEffect } from 'react';
-import { useTheme } from 'next-themes';
-import NextLink from 'next/link';
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import NextLink from "next/link";
 
-import Footer from "./Footer"
+import Footer from "./Footer";
 
 export default function Container(props) {
   const [mounted, setMounted] = useState(false);
@@ -16,11 +16,11 @@ export default function Container(props) {
   const { children, ...customMeta } = props;
   const router = useRouter();
   const meta = {
-    title: 'Jacob Kagon – Developer, writer, creator.',
+    title: "Jacob Kagon – Developer, writer, creator.",
     description: `Front-end developer and JavaScript enthusiast.`,
     // image: 'https://leerob.io/static/images/banner.png',
-    type: 'website',
-    ...customMeta
+    type: "website",
+    ...customMeta,
   };
 
   return (
@@ -29,7 +29,7 @@ export default function Container(props) {
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
         <meta content={meta.description} name="description" />
-      
+
         <meta property="og:type" content={meta.type} />
         <meta property="og:site_name" content="Jacob Kagon" />
         <meta property="og:description" content={meta.description} />
@@ -44,15 +44,15 @@ export default function Container(props) {
           <meta property="article:published_time" content={meta.date} />
         )}
       </Head>
-      <nav className="sticky-nav flex justify-between items-center max-w-4xl w-full p-8 my-0 md:my-8 mx-auto bg-white dark:bg-black bg-opacity-60">
+      <nav className="flex items-center justify-between w-full max-w-4xl p-8 mx-auto my-0 bg-white sticky-nav md:my-8 dark:bg-black bg-opacity-60">
         <a href="#skip" className="sr-only focus:not-sr-only">
           Skip to content
         </a>
         <button
           aria-label="Toggle Dark Mode"
           type="button"
-          className="bg-gray-200 dark:bg-gray-800 rounded p-3 h-10 w-10"
-          onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
+          className="w-10 h-10 p-3 bg-gray-200 rounded dark:bg-gray-800"
+          onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
         >
           {mounted && (
             <svg
@@ -60,9 +60,9 @@ export default function Container(props) {
               viewBox="0 0 24 24"
               fill="currentColor"
               stroke="currentColor"
-              className="h-4 w-4 text-gray-800 dark:text-gray-200"
+              className="w-4 h-4 text-gray-800 dark:text-gray-200"
             >
-              {resolvedTheme === 'dark' ? (
+              {resolvedTheme === "dark" ? (
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
@@ -81,25 +81,29 @@ export default function Container(props) {
           )}
         </button>
         <div>
-          <NextLink href="/dashboard">
-            <a className="p-1 sm:p-4 text-gray-900 dark:text-gray-100">
-              Dashboard
+          <NextLink href="/">
+            <a className="p-1 text-gray-900 sm:p-4 dark:text-gray-100">Home</a>
+          </NextLink>
+          <NextLink>
+            <a
+              href="https://jacob-kagon.medium.com/"
+              target="_blank"
+              className="p-1 text-gray-900 sm:p-4 dark:text-gray-100"
+            >
+              Blog
             </a>
           </NextLink>
-          <NextLink href="/blog">
-            <a className="p-1 sm:p-4 text-gray-900 dark:text-gray-100">Blog</a>
-          </NextLink>
           <NextLink href="/about">
-            <a className="p-1 sm:p-4 text-gray-900 dark:text-gray-100">About</a>
+            <a className="p-1 text-gray-900 sm:p-4 dark:text-gray-100">About</a>
           </NextLink>
           <NextLink href="/">
-            <a className="p-1 sm:p-4 text-gray-900 dark:text-gray-100">Home</a>
+            <a className="p-1 text-gray-900 sm:p-4 dark:text-gray-100">Home</a>
           </NextLink>
         </div>
       </nav>
       <main
         id="skip"
-        className="flex flex-col justify-center bg-white dark:bg-black px-8"
+        className="flex flex-col justify-center px-8 bg-white dark:bg-black"
       >
         {children}
         <Footer />
--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const ExternalLink = ({ href, children }) => (
   <a
-    className="text-gray-500 hover:text-gray-600 transition"
+    className="text-gray-500 transition hover:text-gray-600"
     target="_blank"
     rel="noopener noreferrer"
     href={href}
@@ -13,30 +13,30 @@ const ExternalLink = ({ href, children }) => (
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col justify-center items-start max-w-2xl mx-auto w-full mb-8">
-      <hr className="w-full border-1 border-gray-200 dark:border-gray-800 mb-8" />
+    <footer className="flex flex-col items-start justify-center w-full max-w-2xl mx-auto mb-8">
+      <hr className="w-full mb-8 border-gray-200 border-1 dark:border-gray-800" />
 
-      <div className="w-full max-w-2xl gap-4 grid grid-cols-1 pb-16 sm:grid-cols-2">
-        <div className="flex items-center flex-col space-y-4">
+      <div className="grid w-full max-w-2xl grid-cols-1 gap-4 pb-16 sm:grid-cols-2">
+        <div className="flex flex-col items-center space-y-4">
           <Link href="/">
-            <a className="text-gray-500 hover:text-gray-600 transition">Home</a>
+            <a className="text-gray-500 transition hover:text-gray-600">Home</a>
           </Link>
           <Link href="/projects">
-            <a className="text-gray-500 hover:text-gray-600 transition">
+            <a className="text-gray-500 transition hover:text-gray-600">
               Projects
             </a>
           </Link>
           <Link href="/about">
-            <a className="text-gray-500 hover:text-gray-600 transition">
+            <a className="text-gray-500 transition hover:text-gray-600">
               About
             </a>
           </Link>
         </div>
-        <div className="flex flex-col space-y-4 items-center">
+        <div className="flex flex-col items-center space-y-4">
           <ExternalLink href="https://twitter.com/jacob_kagon">
             Twitter
           </ExternalLink>
-          <ExternalLink href="https://github.com/jacob.kagon">
+          <ExternalLink href="https://github.com/jacobkagon">
             GitHub
           </ExternalLink>
           <ExternalLink href="https://www.linkedin.com/in/jacobkagon/">
