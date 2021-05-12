ed56934209446089fa41e3b9ccd10843c4e5299e
JakesProjects1 <jacob@kagon.net>
2021-05-12 19:12:28 -0400

    adding project data

--- data/projects.js
+++ data/projects.js
@@ -3,19 +3,21 @@ export const projects = [
     name: "Cinematic",
     description:
       "A social movie website where you can see what's popular and what your friends are watching",
-    thumbnail: "images/",
+    thumbnail: "images/Cinematic.png",
     link: "https://cinematic-experience.herokuapp.com/",
   },
   {
-    id: "petrolida-2021",
-    page: "/projects/petrolida-2021",
-    name: "Petrolida 2021",
-    category: "Team of 2",
-    description: "This website is used to promote Petrolida 2021 event.",
-    techStack: ["nextjs", "tailwindcss"],
-    thumbnail: "petrolida.jpg",
-    cloudinaryPublicId: "theodorusclarence/tc-petrolida-2021_bmwmxz.png",
-    link: "http://petrolida.its.ac.id",
-    github: "https://github.com/theodorusclarence/petrolida-2021",
+    name: "Love4Heroes",
+    description:
+      "An app to share appreciation for frontline workers during the Coronavirus. Made during the 2021 uOtta Hackathon.",
+    thumbnail: "images/loveHeroes.png",
+    link: "https://love4heroes.online/",
+  },
+  {
+    name: "The Garden",
+    description:
+      "A journaling app where users can write entries based on prompts",
+    thumbnail: "images/thegarden.png",
+    link: "https://the-nurturing-garden.vercel.app/",
   },
 ];
