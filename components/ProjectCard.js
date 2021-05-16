acab93b7f31fe596bcf05f4dc86ec1d9c780c164
JakesProjects1 <jacob@kagon.net>
2021-05-16 18:42:48 -0400

    done with image grid

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,9 +1,22 @@
 import React from "react";
 import Image from "next/image";
+import { SiGithub, SiYoutube } from "react-icons/si";
+import Link from "next/link";
 
 const ProjectCard = ({ project }) => {
   return (
     <div>
+      <header className="flex justify-between">
+        <h4>
+          <span>{project.name}</span>
+        </h4>
+        <Link target="_blank" href={project.github}>
+          <a>
+            <SiGithub />
+          </a>
+        </Link>
+        <SiYoutube />
+      </header>
       <li
         className="max-w-md rounded-md card project-card md:w-full border-thin ring-vis-0"
         whileHover={{ scale: 1.03, transition: { duration: 0.1 } }}
--- data/projects.js
+++ data/projects.js
@@ -4,31 +4,39 @@ export const projects = [
     name: "Cinematic",
     description:
       "A social movie website where you can see what's popular and what your friends are watching",
-    thumbnail: "images/Cinematic.png",
+    thumbnail: "images/Cinematic_App.png",
     link: "https://cinematic-experience.herokuapp.com/",
+    github: "https://github.com/jacobkagon/cinematic-frontend",
+    youtube: "https://www.youtube.com/watch?v=rsrLsYcfcA0&t=8s",
   },
   {
     id: "love4heroes",
     name: "Love4Heroes",
     description:
       "An app to share appreciation for frontline workers during the Coronavirus. Made during the 2021 uOtta Hackathon.",
-    thumbnail: "images/loveHeroes.png",
+    thumbnail: "images/love4heroesapp.png",
     link: "https://love4heroes.online/",
+    github: "https://github.com/jacobkagon/love4heroes",
+    youtube: "https://www.youtube.com/watch?v=5WN3ZmU_dZ8",
   },
   {
     id: "garden",
     name: "The Garden",
     description:
       "A journaling app where users can write entries based on prompts",
-    thumbnail: "images/thegarden.png",
+    thumbnail: "images/Garden_App.png",
     link: "https://the-nurturing-garden.vercel.app/",
+    github: "",
+    youtube: "",
   },
   {
     id: "atlas",
     name: "Atlas",
     description:
       "A travel log app where you can select countries you've visited and share your stories",
-    thumbnail: "images/atlas_photo.png",
+    thumbnail: "images/Atlas_App.png",
     link: "https://atlas-world.netlify.app/",
+    github: "",
+    youtube: "",
   },
 ];
