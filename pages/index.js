5f97e061c485c30e1c5dd59cfc64c8ff830bd6c5
JakesProjects1 <jacob@kagon.net>
2021-05-31 19:29:47 -0400

    fixed dark mode

--- pages/index.js
+++ pages/index.js
@@ -7,30 +7,28 @@ import About from "../components/About";
 
 export default function Home() {
   return (
-    <div id="home">
-      <Container>
-        <div className="flex flex-col items-start justify-center max-w-2xl mx-auto mb-16">
-          <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
-            Hey, I’m Jacob
-          </h1>
-          <p
-            id="projects"
-            className="mb-16 prose text-gray-600 dark:text-gray-400"
-          >
-            I’m a developer who loves to learn new technologies and make
-            everyday life easier and more enjoyable. Feel free to reach out if
-            you want to collaborate or chat about tech. &nbsp;
-          </p>
-          <h3 className="mb-4 text-2xl font-bold tracking-tight text-black md:text-4xl dark:text-white">
-            My Projects
-          </h3>
-          <div className="py-6 mt-4">
-            <Projects />
-          </div>
-
-          <About />
+    <Container>
+      <div className="flex flex-col items-start justify-center max-w-2xl mx-auto mb-16">
+        <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
+          Hey, I’m Jacob
+        </h1>
+        <h2
+          id="projects"
+          className="mb-16 prose text-gray-600 dark:text-gray-400"
+        >
+          I’m a developer who loves to learn new technologies and make everyday
+          life easier and more enjoyable. Feel free to reach out if you want to
+          collaborate or chat about tech. &nbsp;
+        </h2>
+        <h3 className="mb-4 text-2xl font-bold tracking-tight text-black md:text-4xl dark:text-white">
+          My Projects
+        </h3>
+        <div className="py-6 mt-4">
+          <Projects />
         </div>
-      </Container>
-    </div>
+
+        <About />
+      </div>
+    </Container>
   );
 }
